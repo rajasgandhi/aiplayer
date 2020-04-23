665a23b7003b963a4a8a053d9162bb2d41ef69c8
RajasG <rajasmgandhi@gmail.com>
2020-04-23 09:47:29 -0500

    Update config.php

--- aiplayer/config.php
+++ aiplayer/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_MP3FILE', './mp3/'); #directories ending with /
-define('ROOT_MP3URL', 'https://example.com/aiplayer/mp3/');
+define('ROOT_MP3URL', 'https://nsgamezz.github.io/aiplayer/mp3/');
 
-define('ROOT_LOG', '/var/www/vhosts/example.com/httpdocs/aiplayer/log/');
+define('ROOT_LOG', '/var/www/vhosts/nsgamezz.github.io/httpdocs/aiplayer/log/');
 ?>
